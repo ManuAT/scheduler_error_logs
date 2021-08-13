8b49d4aed08a3d7d1827dafb044b234887817c60
Manu A T <atmanu7@gmail.com>
2021-08-13 18:42:03 +0530

    adding schedular commands

--- src/main/java/com/nectar/failurelogsys/FailurelogsysApplication.java
+++ src/main/java/com/nectar/failurelogsys/FailurelogsysApplication.java
@@ -44,81 +44,63 @@ public class FailurelogsysApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 
-		// =========== testing Database insertion ==========
-
-		// System.out.println("inside run ....");
-		// HistoryData historyData = new HistoryData("Equp2","hello", "11001");
-		// historyRepository.insert(historyData);
-
-		// ErrorLog errorLog = new ErrorLog("Equp2","hello", "11001", "hello");
-		// historyRepository.insertToErrorLog(errorLog);
-
-		// AggregationData aggregationData = new AggregationData("Equp2","hello", "11001");
-		// historyRepository.insertToAggregation(aggregationData);
-
-		// System.out.println("History record inserted successfully in first database");
-
-		// =============================
-		// String startTime,endTime;
-		// LocalDateTime localDate = new LocalDateTime();
-		// localDate = localDate.minusHours(1);
-		// int currentHour = localDate.getHourOfDay();
-		// localDate = localDate.millisOfDay().withMinimumValue();
-		// startTime = localDate.plusHours(currentHour).toString();
-		// endTime = localDate.plusHours(currentHour+1).toString();
-		// System.out.println("StartTime :"+startTime+"EndTime :"+endTime);
-		// System.out.println( historyRepository.selectFromHistoryData(startTime, endTime));
-		// System.out.println( historyRepository.selectFromHistoryData("2021-08-12 07:06:51", "2021-08-12 08:06:51"));
-
 		// ============ job creation and delete =================
+
+		// NOTE: need to command this code after first creation
 
 		HashMap<String, Object> data = new HashMap<String, Object>();
 		data.put("domain", "nectar");
 		taskScheduler.createCroneJob(AGGREGRATION_JOB, AGGREGRATION_JOB, data, EVERY_HOUR,
 					jobObject.class);
 		
+		//NOTE to delete scheduler 
+
 		// taskScheduler.deleteScheduledTask(AGGREGRATION_JOB, AGGREGRATION_JOB);
+
+
+
 
 		// =============== UI setup ==============
 
-	// 	Scanner sc = new Scanner(System.in);
 
-	// 	System.out.println("Enter option");
-	// 	System.out.println("1.Create Weather Prediction Job");
-	// 	System.out.println("2.Delete Weather Prediction Job");
+
+		// 	Scanner sc = new Scanner(System.in);
+
+		// 	System.out.println("Enter option");
+		// 	System.out.println("1.Create Weather Prediction Job");
+		// 	System.out.println("2.Delete Weather Prediction Job");
+			
+		// 	String optionStr = sc.next();
+		// 	int option = Integer.parseInt(optionStr);
+
+		// 	HashMap<String, Object> data = new HashMap<String, Object>();
+
+		// 	String domain = null;
+		// 	if (option == 1 || option == 3) {
+		// 		System.out.println("Enter domain");
+		// 		domain = sc.next();
+		// 		data.put("domain", domain);
+		// 	}
+
+		// 	switch (option) {
+
+		// 		case 1:
+		// 			taskScheduler.createCroneJob(AGGREGRATION_JOB, AGGREGRATION_JOB, data, EVERY_HOUR,
+		// 			jobObject.class);
+		// 			break;
 		
-	// 	String optionStr = sc.next();
-	// 	int option = Integer.parseInt(optionStr);
-
-	// 	HashMap<String, Object> data = new HashMap<String, Object>();
-
-	// 	String domain = null;
-	// 	if (option == 1 || option == 3) {
-	// 		System.out.println("Enter domain");
-	// 		domain = sc.next();
-	// 		data.put("domain", domain);
-	// 	}
-
-	// 	switch (option) {
-
-	// 		case 1:
-	// 			taskScheduler.createCroneJob(AGGREGRATION_JOB, AGGREGRATION_JOB, data, EVERY_HOUR,
-	// 			jobObject.class);
-	// 			break;
-	
-	// 		case 2:
-	// 			taskScheduler.deleteScheduledTask(AGGREGRATION_JOB, AGGREGRATION_JOB);
-	// 			break;
-	// 		default:
-	// 			break;
-	// 		}
+		// 		case 2:
+		// 			taskScheduler.deleteScheduledTask(AGGREGRATION_JOB, AGGREGRATION_JOB);
+		// 			break;
+		// 		default:
+		// 			break;
+		// 		}
 
 
-	// 		sc.close();
-	// 		System.exit(0);
-		
-	// }
-
+		// 		sc.close();
+		// 		System.exit(0);
+			
+		// }
 	
 	}
 
