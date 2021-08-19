550f7c3aadaeacba76907898d342571e37679441
Manu A T <atmanu7@gmail.com>
2021-08-19 15:20:43 +0530

    removing for loop

--- src/main/java/com/nectar/failurelogsys/contoller/ErrorFinderController.java
+++ src/main/java/com/nectar/failurelogsys/contoller/ErrorFinderController.java
@@ -6,7 +6,6 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import com.nectar.failurelogsys.db.model.ErrorArray;
 import com.nectar.failurelogsys.db.model.ErrorLog;
@@ -14,7 +13,6 @@ import com.nectar.failurelogsys.db.model.InputData;
 import com.nectar.failurelogsys.db.model.OutputData;
 import com.nectar.failurelogsys.db.repository.ErrorLogRepository;
 
-import org.hibernate.result.Output;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,18 +37,13 @@ public class ErrorFinderController {
                 List<ErrorLog> resultList = errorLogRepository.selectFromErrorLogData(new Timestamp(inputData.getStartDate()),new Timestamp(inputData.getEndDate()),
                                                     inputData.getClient(), inputData.getEquipments());
 
-                  
                 for(String equip:inputData.getEquipments())
                 {
-                    List<ErrorArray> errorArraysList = new ArrayList<ErrorArray>();
-
-                    List<ErrorLog> resultListUniqe =  resultList.stream().filter(p -> p.getEquipmentName().equals(equip)).collect(Collectors.toList());
-                
-                    // this loop will be removed ..
-                    for(ErrorLog result: resultListUniqe){
-                        errorArraysList.add(new ErrorArray(result.getDate().getTime(),result.getTypeOfFailure(),result.getDescription()));
-                    }
-                    
+                    List<ErrorArray> errorArraysList =  resultList.stream()
+                            .filter(p -> p.getEquipmentName().equals(equip))
+                            .map(p -> new ErrorArray(p.getDate().getTime(), p.getTypeOfFailure(),p.getDescription()))
+                            .collect(Collectors.toList());
+  
                     OutputDataList.add(new OutputData(equip,errorArraysList));
 
                 }
@@ -66,18 +59,12 @@ public class ErrorFinderController {
 
             for(String equip:uniqueEquipment)
             {
-                List<ErrorArray> errorArraysList = new ArrayList<ErrorArray>();
-
-                List<ErrorLog> resultList2 =  resultList.stream().filter(p -> p.getEquipmentName().equals(equip)).collect(Collectors.toList());
-            
-                // this loop will be removed ..
-                for(ErrorLog result: resultList2){
-                    errorArraysList.add(new ErrorArray(result.getDate().getTime(),result.getTypeOfFailure(),result.getDescription()));
-                }
-
+                List<ErrorArray> errorArraysList =  resultList.stream()
+                    .filter(p -> p.getEquipmentName().equals(equip))
+                    .map(p -> new ErrorArray(p.getDate().getTime(), p.getTypeOfFailure(),p.getDescription()))
+                    .collect(Collectors.toList());
 
                 OutputDataList.add(new OutputData(equip,errorArraysList));
-
             }
             
             }
