a41023408911983f500a3eff4d1157015cbba83e
Manu A T <atmanu7@gmail.com>
2021-08-19 18:12:32 +0530

    adding swagger

--- src/main/java/com/nectar/failurelogsys/contoller/ErrorFinderController.java
+++ src/main/java/com/nectar/failurelogsys/contoller/ErrorFinderController.java
@@ -2,11 +2,12 @@ package com.nectar.failurelogsys.contoller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import com.nectar.failurelogsys.db.model.ErrorArray;
@@ -15,7 +16,6 @@ import com.nectar.failurelogsys.db.model.InputData;
 import com.nectar.failurelogsys.db.model.OutputData;
 import com.nectar.failurelogsys.db.repository.ErrorLogRepository;
 
-import org.hibernate.result.Output;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,10 +24,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class ErrorFinderController {
+
+    private final String LIST_SUCCESS_RESP = "LIST_SUCCESS_RESP";
+    private final String GENERAL_FIELD_NOT_SPECIFIED = "GENERAL_FIELD_NOT_SPECIFIED";
+    private final String GENERAL_FIELD_INVALID = "GENERAL_FIELD_INVALID";
+    
     @Autowired
     ErrorLogRepository errorLogRepository;
 
     @GetMapping("/equip")
+    @ApiResponse(responseCode = "200", description = LIST_SUCCESS_RESP)
+    @ApiResponse(responseCode = "505", description = GENERAL_FIELD_NOT_SPECIFIED)
+    @ApiResponse(responseCode = "508", description = GENERAL_FIELD_INVALID)
     public List<OutputData>  getErrors(@RequestBody InputData inputData) {
 
 
